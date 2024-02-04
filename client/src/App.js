d9213124953719d86a9709f70693f014b94aa051
mkc-c <abdirisak.arr@gmail.com>
2024-02-04 20:30:25 +0000

    correct typo

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage"></Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
