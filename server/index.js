520139ff39fbf5d09795bd62a2fea0b18f1c2aae
mkc-c <abdirisak.arr@gmail.com>
2024-02-04 16:15:08 +0000

    correct typo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const keys = require("./keys");
 
 const express = require("express");
-const bodyParser = requier("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
