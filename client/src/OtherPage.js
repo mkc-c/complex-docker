b9aee836e7db1a32e7e59242cf42e94c9ecc1979
mkc-c <abdirisak.arr@gmail.com>
2024-02-04 12:49:23 +0000

    create client

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      I'm some other page!
+      <Link>Go back home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
